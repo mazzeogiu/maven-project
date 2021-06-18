1251478bb47348885d1fbb802f7dbc04491b94cd
mazzeogiu <65913188+mazzeogiu@users.noreply.github.com>
2021-06-18 12:14:39 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -3,22 +3,22 @@ package com.example;
 
 /**
 * <p>
-* Class main
-* Please see the {@link com.baeldung.javadoc.Person} class for true identity
+* Class main.
+* Please see the {@link com.baeldung.javadoc.Person} class for true identity.
 * @author Captain America
 * @version 1.0
 * @since 18/06/2021
 * </p>
-* This method always returns a welcome message
+* This method always returns a welcome message.
 */
 public class Greeter {
 /**
-*Cretes an empty constructor
+*Cretes an empty constructor.
 */
   public Greeter() {
 
   }
-/** Method return "hello"
+/** Method return "hello".
 * @param someone nom de l'utilisateur
 * @return string welcome message
 */
